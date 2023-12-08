3a15f079822fb77e9f4c90691fbe492c8e05cddf
boredbread <kerteszlevi@gmail.com>
2023-12-08 15:18:50 +0100

    rfr

--- main.py
+++ main.py
@@ -1,16 +1,42 @@
-# This is a sample Python script.
+import pandas as pd
+from sklearn.preprocessing import MinMaxScaler
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.preprocessing import PolynomialFeatures
 
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+x_train = pd.read_csv('dataset_4e732c/housing_x_train_4e732c.csv', sep=',', encoding='utf-8').values
+y_train = pd.read_csv('dataset_4e732c/housing_y_train_4e732c.csv', sep=',', encoding='utf-8').values
+
+scaler_x = MinMaxScaler()
+scaler_y = MinMaxScaler()
+
+scaler_x.fit(x_train)
+normalized_x_train = scaler_x.transform(x_train)
+
+scaler_y.fit(y_train)
+normalized_y_train = scaler_y.transform(y_train)
+
+x_test = pd.read_csv('dataset_4e732c/housing_x_test_4e732c.csv', sep=',', encoding='utf-8').values
+
+normalized_x_test = scaler_x.transform(x_test)
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
+poly = PolynomialFeatures(degree=2)
+normalized_x_train_poly = poly.fit_transform(normalized_x_train)
+normalized_x_test_poly = poly.transform(normalized_x_test)
+
+##using rf
+
+random_forest = RandomForestRegressor(n_estimators=50, random_state=42, n_jobs=-1)
+
+random_forest.fit(normalized_x_train_poly, normalized_y_train.ravel())
+
+predictions_rf = random_forest.predict(normalized_x_test_poly)
+
+reverted_rf_predictions = scaler_y.inverse_transform(predictions_rf.reshape(-1, 1))
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+predictions_df = pd.DataFrame(reverted_rf_predictions)
+predictions_df.to_csv('housing_y_test.csv', sep=',', encoding='utf-8', index=False)
+#np.savetxt('housing_y_test.csv', reverted_ridge_predictions, delimiter=",", fmt="%g")
+
